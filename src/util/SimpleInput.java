b28ba0cb1e5c73d66519495fba7050c9391b78fb
ckk914 <ckk9114@naver.com>
2024-04-18 17:26:04 +0900

    스캐너 static 처리

--- src/util/SimpleInput.java
+++ src/util/SimpleInput.java
@@ -4,13 +4,24 @@ import java.util.Scanner;
 
 //역할: 스캐너 입력을 간소화 해주는 객체
 public class SimpleInput {
-    Scanner sc = new Scanner(System.in);
+    private static Scanner sc;
+
+    static{
+    sc = new Scanner(System.in);
+    }
+
 
     //문자열 입력을 처리
-    public String input(String message) {
+    public static String input(String message) {
         System.out.print(message);
         return sc.nextLine();
     }
+//    public static  Simpleinput(String message) {
+//        Scanner sc1 = new Scanner(System.in);
+//        System.out.print(message);
+//        return sc1.nextLine();
+//    }
+    
     // 엔터를 누르기 전까지 멈춰 있는 기능
     public void stopInput() {
         System.out.println("\n========= 엔터를 누르면 계속 … ========");
